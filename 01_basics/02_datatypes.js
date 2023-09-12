9efc92f89885ca39f5daf4e6972f5dbb6797bd47
AbdullaMulla07 <144640934+AbdullaMulla07@users.noreply.github.com>
2023-09-12 16:55:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all Js code as newer version
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) //code readability should be high - (kyunki agar code readable na ho
+        //toh baad mein use change yaa phir samaj nai aaye ga)
+
+        //TYPES OF DATATYPES
+        //PRIMITIVE DATATYPE
+//number => 2 to the power of 53 - (yeh range hota jitna aapke memory address mein milta hai)
+//bigint => agar aapka bhot zyada bada hai toh bigint use karte hai(ex: trading mein, stock market mein, ya phir badi websites mein) - (zyada tar aap apni coding life bigint ko utna use nahi karenge)
+//string => use in double quotes ("") prefer karein
+//boolean => true / false
+//null => standlone value hai - (null ek object hai)
+//undefined => (ex: let state;)
+//symbol => to use uniqueness symbol is been used 
+        //NON PRIMITIVE DATATYPE
+//object =>  
