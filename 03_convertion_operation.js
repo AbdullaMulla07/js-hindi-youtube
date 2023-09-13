e8a2f0f604bbe49371f72468e2380a29dff153c3
AbdullaMulla07 <144640934+AbdullaMulla07@users.noreply.github.com>
2023-09-13 02:00:48 +0000

    convertion_operation

---
+++ 03_convertion_operation.js
@@ -0,0 +1,5 @@
+let score = 33 //- (sab se pehle aapko iska type check karna hoga kyunki kai baar aap backend mein kaam karrahe hote hai )
+
+//there are 2 types to check type of any datatypes
+console.log(typeof score);
+console.log(typeof(score));
