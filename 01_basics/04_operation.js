0ddc0a27bfa7c0b63c507841b3ee0ba12ea3d198
AbdullaMulla07 <144640934+AbdullaMulla07@users.noreply.github.com>
2023-09-13 13:39:02 +0000

    operation

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,15 @@
+// 7th VIDEO OF CHAI AUR CODE 
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(3 + 4 * 5 % 3);
+//👆👆 aise na samajne wale code nahi likna hai, only clear cut code likna hai 👆👆
+
