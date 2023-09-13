c287ff091db39d9ed89ddbb44f75b731baf0fbb0
AbdullaMulla07 <144640934+AbdullaMulla07@users.noreply.github.com>
2023-09-13 14:41:42 +0000

    datatypes_summary

---
+++ 01_basics/06_datatypes_summary.js
@@ -0,0 +1,20 @@
+// 9th VIDEO OF CHAI AUR CODE
+
+// In the Official Document the datatypes are catogrised into two types
+// 1. Primitive datatype:-
+//               7 types: String, Number, Boolean, null(iska matlab hai empty or zero nahi hai), 
+//                        undefined(aapne variable declare kardiya hai but usmein aap kya value dalenge aap decide kar rahe hai), 
+//                        Symbol(kisi bhi value ko unique banane ke liye use kiya jata hai), BigInt(jo badi values hoti hai oo hum BigInt mein use karte hai)
+
+// 2. Non Primitive datatype or reference datatype
+//               Array
+              const fruits = ["Mango", "Orange", "Apple"]
+//               Objects
+              const bio_data = {
+                name: "Abdulla",
+                age: 18,
+              }
+//               Functions
+              const my_Function = function(){
+                console.log("Hello World");
+              }
