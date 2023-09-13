90381b71cb342b3f9a0423c7d1e3ef48a8dc4dcb
AbdullaMulla07 <144640934+AbdullaMulla07@users.noreply.github.com>
2023-09-13 14:03:46 +0000

    comparison

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,27 @@
+// 8th VIDEO OF CHAI AUR CODE
+
+//****************************👇👇👇👇👇*************************************//
+
+// Comparison or quality difference dono alag cheeze hai 
+
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+// 👆👆 yeh basic conversion hai yaha tak koi problem nai hai
+
+console.log("2" > 1);
+console.log("02" > 1);
+// 👆👆 yaha pe fasne ke chances bhot hai kyunki(idar ek datatype hai or udar ek)
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+// 👆👆 jaise hi aap null convert karenge kabhi oo nan ke andar karta hai kabhi oo 0 andar karta hai
+
+console.log("2" === 2);
+// (===)na ki aapke values check karta hai balki strictly aapke values ko check karta hai(yaani uske datatype ko bhi check karta hai)
+
