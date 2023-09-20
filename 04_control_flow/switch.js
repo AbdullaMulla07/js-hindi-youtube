f44874983e53eb5a4a820e1f3ca53916a8fe962c
AbdullaMulla07 <144640934+AbdullaMulla07@users.noreply.github.com>
2023-09-20 09:19:29 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,21 @@
+//continuation of 26th VIDEO OF CHAI AUR CODE
+
+const month = 3
+switch (month) { // what is key => oo value jo mujhe har baar check karni hai
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Februray");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
