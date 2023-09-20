b4cb5438bec9812a58e0a31ca7c1cd23b9995c8d
AbdullaMulla07 <144640934+AbdullaMulla07@users.noreply.github.com>
2023-09-20 12:28:46 +0000

    two

---
+++ 05_iterations_loops/two.js
@@ -0,0 +1,27 @@
+//28th VIDEO OF CHAI AUR CODE 
+
+//while loop
+
+let index = 0
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+
+let myArray = ["Flash", "Batman", "Superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+// do while loop
+
+let score = 1
+do {            //(pehle aap kaam hoga than condition check hogi)
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
