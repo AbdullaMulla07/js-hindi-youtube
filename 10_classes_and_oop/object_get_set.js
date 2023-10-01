37fef6c576f537868ebb3b8afb1fb1ee3d499a69
AbdullaMulla07 <144640934+AbdullaMulla07@users.noreply.github.com>
2023-10-01 01:22:11 +0000

    objectgetset

---
+++ 10_classes_and_oop/object_get_set.js
@@ -0,0 +1,20 @@
+// continuation of 48 VIDEO OF CHAI AUR CODE
+
+// object get and set
+
+const User = {
+    _email: 'h@hc.com',
+    _password: "abc",
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+const tea = Object.create(User)
+console.log(tea.email);
